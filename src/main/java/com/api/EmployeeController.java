ad9be42c3d2124e0e5d5cd064bd977512145bf1a
sjabiulla <s.jabiulla@gmail.com>
2021-02-01 10:57:14 +0530

    Revert "Added DB changes. Using H2 for now."
    This reverts commit a964a7d3cd6ddf02c47069bab5a592a4952d7f6f.

--- src/main/java/com/api/EmployeeController.java
+++ src/main/java/com/api/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,41 +13,64 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 
-	@Autowired
-	private EmployeeRepository repo;
 
 	@PostMapping("employees")
-	public ResponseEntity<Employee> createUser(@RequestBody Employee employee) {
-		System.out.println("Empl Name: " + employee.getName());
-		employee = repo.save(employee);
-		return ResponseEntity.status(HttpStatus.CREATED).body(employee);
+	public ResponseEntity createUser( @RequestBody Employee employee ){
+		
+		System.out.println("Empl Name: "+ employee.getName());
+		
+		// Do business logic
+		
+//		return ResponseEntity.status(HttpStatus.CREATED).build();
+		return new ResponseEntity(HttpStatus.CREATED);
 	}
-
+	
 	@GetMapping("/employees")
-	public ResponseEntity<?> getUsers() {
+	public ResponseEntity getUsers( ){
+		
 		System.out.println("Retrieving all emp ");
-		return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
+		
+		// Do business logic
+		
+//		return ResponseEntity.status(HttpStatus.CREATED).build();
+		return new ResponseEntity(HttpStatus.CREATED);
 	}
-
+	
 	@GetMapping("/employees/{employeeId}")
-	public ResponseEntity<Employee> getUser(@PathVariable long employeeId) {
-		System.out.println("Retrieving emp " + employeeId);
-		return ResponseEntity.status(HttpStatus.OK).body(repo.findById(employeeId).get());
+	public ResponseEntity getUser( @PathVariable long employeeId ){
+		
+		System.out.println("Retrieving emp "+employeeId);
+		
+		// Do business logic
+		
+//		return ResponseEntity.status(HttpStatus.CREATED).build();
+		return new ResponseEntity(HttpStatus.CREATED);
 	}
-
+	
 	@PutMapping("/employees/{employeeId}")
-	public ResponseEntity<Employee> updateUser(@RequestBody Employee employee, @PathVariable long employeeId) {
-		System.out.println("Updating : " + employeeId);
-		employee.setId(employeeId);
-		employee = repo.save(employee);
-		return ResponseEntity.status(HttpStatus.CREATED).body(employee);
-	}
+	public ResponseEntity updateUser( @RequestBody Employee employee, @PathVariable long employeeId ){
+		
+		System.out.println("Updating : "+ employeeId);
 
+		// Do business logic
+		
+//		return ResponseEntity.status(HttpStatus.CREATED).build();
+		return new ResponseEntity(HttpStatus.CREATED);
+	}
+	
 	@DeleteMapping("/employees/{employeeId}")
-	public ResponseEntity<?> deleteUser(@PathVariable long employeeId) {
-		System.out.println("Deleting " + employeeId);
-		repo.deleteById(employeeId);
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-	}
+	public ResponseEntity deleteUser(@PathVariable long employeeId ){
+		
+		System.out.println("Deleting "+employeeId);
 
+		// Do business logic
+		
+//		return ResponseEntity.status(HttpStatus.CREATED).build();
+		return new ResponseEntity(HttpStatus.CREATED);
+	}
+	
+	
+
+	
+	
 }
--- src/main/java/com/api/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
-
-}
